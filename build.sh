6142724985c7868d7a929f851ec665377e01c9a4
Dhruv N Patel <dhruv@weam.ai>
2025-09-22 13:05:15 +0530

    Update build.sh, docker-compose.yml and add minio.sh after remove ai-python

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ echo "✅ Docker Compose Command: $COMPOSE_CMD"
 
 # Step 1: Build base image
 echo "🚧 Step 1/5: Building base image (pybase_docker)..."
-$COMPOSE_CMD build --no-cache pybase_docker || { echo "❌ Failed to build pybase_docker"; exit 1; }
+# $COMPOSE_CMD build --no-cache pybase_docker || { echo "❌ Failed to build pybase_docker"; exit 1; }
 echo "✅ pybase_docker image built successfully."
 
 # Step 2: Load .env
